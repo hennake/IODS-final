9cf775b625cfca20485240db5de0e2cd69e50bf8
hennake <henna.kettunen@kapsi.fi>
2017-03-07 00:21:25 +0200

    First update

---
+++ final.r
@@ -0,0 +1,102 @@
+# IODS final assignment
+# Henna Kettunen
+
+setwd("L:\\Users\\Henna\\storage\\r\\iods\\IODS-final\\IODS-final")
+
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+
+load("tree_data.RData")
+
+# Summary
+summary(dat)
+
+# Histograms
+library(reshape2)
+mdat <- melt(dat)
+ggplot(mdat, aes(value)) + geom_histogram(fill="#009E73") + facet_wrap(~ variable, scales="free")
+
+# Scatterplot matrix
+pairs(dat, cex=0.1, pch=16, col=dat$Genus)
+
+# Correlation matrix
+library(corrplot)
+cor_matrix <- cor(dat[,2:ncol(dat)]) 
+corrplot(cor_matrix, method="circle", type="upper", cl.pos="b", tl.pos="d", tl.cex=0.6)
+
+# Drop correlated variables
+drop <- c("leaf_toughness", "C_N", "SLA")
+dat <- dat[,!colnames(dat) %in% drop]
+
+
+## Multinomial logistic regression
+
+# Divide to training and test sets
+# ind <- sample(nrow(dat), nrow(dat)*0.5)
+# save(ind, file="ind.RData")
+load("ind.RData")
+train <- dat[ind,]
+test <- dat[-ind,]
+
+# Full main effects model
+library(nnet)
+fit1 <- multinom(Genus ~ ., data = train)
+pred1 <- predict(fit1, newdata=test)
+genus <- test$Genus
+tab <- table(pred=pred1, obs=genus)
+tab2 <- as.data.frame(tab)
+# Correct rate
+cor1 <- sum(tab2$Freq[tab2$pred==tab2$obs])/nrow(test)
+tab
+corg <- tab2[tab2$pred==tab2$obs,c("pred","Freq")]
+cor1
+
+
+# Model selection
+library(MASS)
+step <- stepAIC(fit1, direction="backward")
+
+# Best model according to AIC
+fit2 <- multinom(Genus ~ leaf_thickness + sapwood_density + N + C13 + chlorophyll_concentration + surface_area, data = train)
+pred2 <- predict(fit2, newdata=test)
+pred2p <- as.data.frame(fitted(fit2))
+tab3 <- table(pred=pred2, obs=genus)
+tab4 <- as.data.frame(tab3)
+# Correct rate
+cor2 <- sum(tab4$Freq[tab4$pred==tab4$obs])/nrow(test)
+cor2
+
+# Model summary and odd ratios by exponentation
+summary(fit2)
+exp(coef(fit2))
+
+# 2-tailed Wald test
+z <- summary(fit2)$coefficients/summary(fit2)$standard.errors
+p <- (1 - pnorm(abs(z), 0, 1)) * 2
+
+
+# Full model with 2-way interactions
+fit3 <- multinom(Genus ~ .*., data = train)
+pred1 <- predict(fit1, newdata=test)
+AIC(fit1, fit2, fit3)
+
+coef(fit2)
+library(AER)
+coeftest(fit2)
+
+fit4 <- multinom(Genus ~ leaf_thickness + sapwood_density + N + chlorophyll_concentration + surface_area, data = train)
+AIC(fit4)
+
+trainp <- predict(fit2)
+tabp <- table(pred=trainp, obs=train$Genus)
+tabp2 <- as.data.frame(tabp)
+# Correct rate
+corp <- sum(tabp2$Freq[tabp2$pred==tabp2$obs])/nrow(test)
+corp
+
+# Visualization
+library(visreg)
+visreg(fit2, xvar="leaf_thickness", collapse=T, type="conditional")
+
+
---
+++ wrangle.r
@@ -0,0 +1,75 @@
+# IODS final assignment
+# Henna Kettunen
+
+setwd("L:\\Users\\Henna\\storage\\r\\iods\\IODS-final")
+
+library(tidyr)
+library(dplyr)
+
+# Tropical tree leaf data from BRIDGE database obtained via TRY
+trees <- read.csv("trees.csv")
+str(trees)
+
+# Combine genus and species to a same column
+trees$name <- paste0(trees$Genus, "_", trees$species)
+
+# How many families? 59
+famn <- summarise(trees, nfam=n_distinct(Family))
+
+# How many genera? 222
+genn <- summarise(trees, nfam=n_distinct(Genus))
+
+# How many species? 646
+specn <- summarise(trees, nfam=n_distinct(name))
+
+# How many genera per family?
+ngen <- trees %>% group_by(Family) %>% summarise(ngen=n_distinct(Genus))
+
+# How many species per genus?
+nspec <- trees %>% group_by(Genus) %>% summarise(nspec=n_distinct(species))
+
+# How many observations per species?
+nobs <- count(trees[complete.cases(trees)==T,], name)
+nobs <- arrange(nobs, desc(n))
+nobs[1:10,]
+
+# How many observations per genus?
+nobsg <- trees[complete.cases(trees),] %>% group_by(Genus) %>% summarise(nobs=n())
+nobsg <- arrange(nobsg, desc(nobs))
+
+# Genera with >= 50 observations
+gen50 <- nobsg[which(nobsg$nobs>=50),]
+gen50 <- gen50[gen50$Genus!="IND",]
+
+# Which families do these belong to?
+fam50 <- unique(trees[trees$Genus %in% gen50$Genus, c("Family","Genus")])
+fam50$n.obs <- gen50$nobs[match(fam50$Genus, gen50$Genus)]
+
+# 6 genera with >=50 complete cases belonging to 4 different families selected for further analyses
+dat <- trees[complete.cases(trees) & trees$Genus %in% gen50$Genus,]
+specn2 <- length(unique(dat$name))
+
+# Number of species per genus
+nspec2 <- dat %>% group_by(Genus) %>% summarise(nspec=n_distinct(species))
+fam50$n.spec <- nspec2$nspec[match(fam50$Genus, nspec2$Genus)]
+
+# Select columns
+keep <- c("Genus","DBH","leaf_thickness","leaf_toughness","sapwood_density","N","C_N","N15","C13","chlorophyll_concentration","surface_area","SLA")
+dat <- dat[,keep]
+
+# # Standardize the data
+# dat2 <- dat
+# dat[,2:ncol(dat)] <- scale(dat[,2:ncol(dat)])
+# summary(dat)
+
+dat$Genus <- droplevels(dat$Genus)
+save(dat, file="tree_data.RData")
+
+
+## Should the analysis be done on family-level after all
+# dat3 <- trees[complete.cases(trees) & trees$Genus %in% gen50$Genus,]
+# keep3 <- c("Family","DBH","leaf_thickness","leaf_toughness","sapwood_density","N","C_N","N15","C13","chlorophyll_concentration","surface_area","SLA")
+# dat3 <- dat3[,keep3]
+# dat3$Family <- droplevels(dat3$Family)
+# pairs(dat3, cex=0.1, pch=16, col=dat3$Family)
+
